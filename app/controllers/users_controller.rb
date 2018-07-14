1e01f4e9f22fa18c286cca6ece8acf2e32a1bba9
juank2586 <juanca2586@gmail.com>
2018-07-14 16:28:41 -0500

    Actualización Proyecto Albeiro

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,53 @@
+class UsersController < ApplicationController
+    def index
+        render 'index'
+    end
+    def login
+        if User.exists?(:login=>params[:txtUser],:password=>params[:txtPassword])
+            @mensaje="Usuario y clave correcto"
+            @tipo="success"
+            render 'home',layout: 'home'
+        else
+            @mensaje="Usuario o clave incorrecto"
+            @tipo="warning"
+            render 'index'
+        end
+        
+    end
+    def register
+        @perfiles=Profile.all
+        render 'register',layout: 'home'
+    end
+    def save_register
+        @perfiles=Profile.all
+        if !params[:txtPassword].strip.eql?(params[:txtConfirmarPassword].strip)
+            @mensaje="Password no coincide"
+            @tipo="warning"
+            render 'register',layout: 'home'
+            return
+        end
+
+        user_search_id=0
+        id=0
+        user_search_id=User.order("id").last
+        id=user_search_id.id+1
+
+        user=User.new
+        user.id=id
+        user.login=params[:txtLogin]
+        user.password=params[:txtPassword]
+        user.perfil_id=params[:ddlPerfiles]
+        
+        if user.save
+            @mensaje="Usuario creado correctamente"
+            @tipo="success"
+            @perfiles=Profile.all
+            render 'register',layout: 'home'
+        else
+            @mensaje="Error al crear el usuario"
+            @tipo="warning"
+            @perfiles=Profile.all
+            render 'register',layout: 'home'
+        end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :profiles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #Crear un alias, con la asignacion le indico el controlador y accion.
+  get "users" => "users#index"
+  post "users/login"
+  get "users/register"
+  post "users/save_register"
 end
