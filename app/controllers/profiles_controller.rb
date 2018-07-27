ec9f8a6a1415faa588cc6e7ce2f140eb74627e9f
albeirovalenciag <albeiro.valenciag@gmail.com>
2018-07-27 13:05:19 -0500

    Campo fecha creacion automatico

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -25,7 +25,7 @@ class ProfilesController < ApplicationController
   # POST /profiles
   # POST /profiles.json
   def create
-    @profile = Profile.new    
+    @profile = Profile.new(profile_params)
 
     respond_to do |format|
       if @profile.save
