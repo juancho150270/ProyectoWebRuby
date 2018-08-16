28c94b14a546a0d433f5aec3760a2e7c13f2ff78
juank2586 <juanca2586@gmail.com>
2018-08-16 17:20:09 -0500

    Actualización Login y Restablecer Clave.

--- public/principal/js/login.js
+++ public/principal/js/login.js
@@ -2,21 +2,35 @@
 
 $(function() {
     var button = $('#loginButton');
+    var btnRecovery = $('#recoveryButton');
     var box = $('#loginBox');
+    var boxRecovery = $('#recoveryBox');
     var form = $('#loginForm');
+    var rForm = $('#rloginForm');
+    boxRecovery.hide();
     button.removeAttr('href');
     button.mouseup(function(login) {
         box.toggle();
         button.toggleClass('active');
         button.hide();
     });
+    btnRecovery.removeAttr('href');
+    btnRecovery.mouseup(function(rLogin) {
+        boxRecovery.toggle();
+        btnRecovery.toggleClass('active');
+        box.hide();
+    });
     form.mouseup(function() { 
+        return false;
+    });
+    rForm.mouseup(function() { 
         return false;
     });
     $(this).mouseup(function(login) {
         if(!($(login.target).parent('#loginButton').length > 0)) {
             button.removeClass('active');
             box.hide();
+            boxRecovery.hide();
             button.show();
         }
     });
